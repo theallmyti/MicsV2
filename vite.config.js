866de4d0df73cfc6ea56d1479af2e26fd71b74c7
Aditya Prasad <adityapprrap@gmail.com>
2026-05-28 18:47:19 +0530

    Convert website to a PWA

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,41 @@ import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
 import path from 'path'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Mics',
+        short_name: 'Mics',
+        description: 'Music Streaming App',
+        theme_color: '#121212',
+        background_color: '#121212',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       react: path.resolve(__dirname, 'node_modules/react'),
